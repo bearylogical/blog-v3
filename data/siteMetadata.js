01691edfa7b52387b6ef3b240080c5bcccd028f2
Syamil Maulod <syamil8@gmail.com>
2025-05-12 21:10:30 +0800

    updated metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'notes from a tiny red dot on science, technology, and stuff',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://bearylogical.net',
+  siteRepo: 'https://github.com/bearylogical/blog-v3/',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
